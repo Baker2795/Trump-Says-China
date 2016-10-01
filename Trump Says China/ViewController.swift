cb5cf24c0bf86b1ec400cbd8424bee2b62beae2a
John Baker <baker@johns-macbook-pro.local>
2016-10-01 15:14:59 -0400

    Initial Commit

---
+++ Trump Says China/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trump Says China
+//
+//  Created by John Baker on 10/1/16.
+//  Copyright © 2016 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
